4fdba3c7bfe9533805cce741fcaf7cba9061f7bc
Muaz Yediyüzkırkiki <39378729+muaz742@users.noreply.github.com>
2019-07-11 16:52:41 +0300

    fonksiyonlar geliştirildi
    işlemler sadeleştirildi
    işleyiş yeniden geliştirildi
    
    https://github.com/muaz742/mod62/issues/1
    soruna sebep olan prensip hatası giderildi

--- mod62.php
+++ mod62.php
@@ -5,92 +5,43 @@
  * Geliştirici: muaz
  */
 
-function mod62_encode($girdi)
+function mod62_encode($girdi, $anahtar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
 {
-    /** anahtar diziyi tanımla */
-    $anahtar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    /** sayaç değerini 0 olarak tanımla */
+    $bolunen = $girdi;
+    $anahtar = str_split($anahtar);
     $i = 0;
-    /** kalanlar dizisini oluştur */
     $kalanlar = [];
-    /** anahtar dizisi eleman sayısına bir ekle ve bölen değişkenine tanımla */
-    $bolen = count($anahtar) + 1;
-    /** eğer girdi, bölenden küçükse */
-    if ($girdi < $bolen) {
-        /** girdiyi kalanlar dizisine ekle */
-        array_push($kalanlar, $girdi);
-    }
-    /** girdi, bölenden büyük oldukça çalıştır */
-    while ($girdi > $bolen) {
-        /** girdinin bölene bölümünden kalanı kalana tanımla */
-        $kalan = $girdi % $bolen;
-        /** girdiden kalanı çıkar ve girdiye tanımla */
-        $girdi = $girdi - $kalan;
-        /** girdiyi bölene böl */
-        $girdi = $girdi / $bolen;
-        /** kalanı kalanlar dizisine ekle */
+    $bolen = count($anahtar);
+    while ($bolunen > 1) {
+        $kalan = $bolunen % $bolen;
+        $bolum = floor($bolunen / $bolen);
         array_push($kalanlar, $kalan);
-        /** eğer girdi, bölenden küçükse */
-        if ($girdi < $bolen) {
-            /** girdiyi kalanlar dizisine ekle */
-            array_push($kalanlar, $girdi);
-        }
-        /** sayaç değerini 1 arttır */
+        $bolunen = $bolum;
         $i++;
     }
-    /** sayaç değerini 0 olarak tanımla */
-    $i = 0;
-    /** sayaç değeri kalanlar dizisinin eleman sayısından küçük oldukça çalıştır */
-    while ($i < count($kalanlar)) {
-        /** eğer çıktı değeri tanımlı değilse */
-        if (!isset($cikti)) {
-            /** kalana karşılık gelen değeri çıktıya tanımla */
-            $cikti = $anahtar[$kalanlar[$i] - 1];
-        } else {
-            /** kalana karşılık gelen sonucu çıktının sonuna ekle */
-            $cikti = $cikti . $anahtar[$kalanlar[$i] - 1];
-        }
-        /** kontrol değerini 1 arttır */
-        $i++;
+    $i = count($kalanlar) - 1;
+    while ($i >= 0) {
+        $degerA = $anahtar[$kalanlar[$i]];
+        $i--;
+        $cikti = $cikti . $degerA;
     }
     /** çıktıyı döndür */
     return $cikti;
 }
 
-function mod62_decode($girdi)
+function mod62_decode($girdi, $anahtar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
 {
-    /** anahtar diziyi tanımla */
-    $anahtar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    /** girdiyi karakterlerine böl ve diziye çevir */
+    $anahtar = str_split($anahtar);
+    $bolen = count($anahtar);
     $girdi = str_split($girdi);
-    /** anahtar dizisinin indis ve değerlerini yer değiştir */
+    $girdi = array_reverse($girdi);
     $anahtar = array_flip($anahtar);
-    /** sayaç değerini 0 olarak tanımla */
     $i = 0;
-    /** kalanlar dizisini oluştur */
-    $kalanlar = [];
-    /** sayaç değeri, girdi dizisi eleman sayısından küçük oldukça çalıştır */
     while ($i < count($girdi)) {
-        /** anahtar dizisinde girdiye karşılık gelen sonucu kalanlar dizisine ekle */
-        array_push($kalanlar, $anahtar[$girdi[$i]] + 1);
-        /** sayaç değerini 1 arttır */
-        $i++;
-    }
-    /** anahtar dizisinin eleman sayısının 1 fazlasını çarpan değişkenine tanımla */
-    $carpan = count($anahtar) + 1;
-    /** sayaç değerini 0 olarak tanımla */
-    $i = 0;
-    /** sayaç değeri, kalanlar dizisinin elaman sayısından küçük oldukça çalıştır */
-    while ($i < count($kalanlar)) {
-        /** çarpan üzeri sayaç değeri işleminin sonucunu degerA değişkenine tanımla */
-        $degerA = pow($carpan, $i);
-        /** kalanlar dizisinde sayaç değerine karşılık gelen değeri degerB değişkenine tanımla */
-        $degerB = $kalanlar[$i];
-        /** degerA ile degerB değerlerini çarp islem değişkenine tanımla */
-        $islem = $degerA * $degerB;
-        /** eğer çıktı değeri boşsa; çıktı değerini işlem değerinden al değilse çıktı değerine işlem değerini ekle */
-        (empty($cikti)) ? $cikti = $islem : $cikti = $cikti + $islem;
-        /** sayaç değerini 1 arttır */
+        $kalan = $anahtar[$girdi[$i]];
+        $taban = pow($bolen, $i);
+        $bolunen = $kalan * $taban;
+        $cikti = $cikti+$bolunen;
         $i++;
     }
     /** çıktıyı döndür */
