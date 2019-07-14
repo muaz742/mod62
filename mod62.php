57d44df2d6eef78b81d6e1b2673ff0c40dfdad59
Muaz Yediyüzkırkiki <39378729+muaz742@users.noreply.github.com>
2019-07-14 21:26:45 +0300

    fonksiyonlar Mod62 sınıfı içeirsinde birleştirildi

--- mod62.php
+++ mod62.php
@@ -5,56 +5,65 @@
  * Geliştirici: muaz
  */
 
-function mod62_encode($girdi, $anahtar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+class Mod62
 {
-    /** anahtar diziyi tanımla */
-    $anahtar = str_split($anahtar);
-    /** bölüneni tanımla */
-    $bolunen = $girdi;
-    /** böleni tanımla */
-    $bolen = count($anahtar);
-    /** kalanları tanımla */
-    $i = 0;
-    $kalanlar = [];
-    while ($bolunen > 1) {
-        $kalan = $bolunen % $bolen;
-        $bolum = floor($bolunen / $bolen);
-        array_push($kalanlar, $kalan);
-        $bolunen = $bolum;
-        $i++;
-    }
-    /** karakterleri tanımla */
-    $i = count($kalanlar) - 1;
-    while ($i >= 0) {
-        $degerA = $anahtar[$kalanlar[$i]];
-        $i--;
-        /** karşılık gelen değeri çıktıya ekle */
-        (empty($cikti))?$cikti=$degerA:$cikti = $cikti . $degerA;
-    }
-    /** çıktıyı döndür */
-    return $cikti;
-}
+    public $girdi;
+    public $anahtar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
-function mod62_decode($girdi, $anahtar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-{
-    /** anahtar diziyi tanımla */
-    $anahtar = str_split($anahtar);
-    $anahtar = array_flip($anahtar);
-    /** böleni tanımla */
-    $bolen = count($anahtar);
-    /** karakterleri tanımla */
-    $girdi = str_split($girdi);
-    $girdi = array_reverse($girdi);
-    /** sayıyı hesapla */
-    $i = 0;
-    while ($i < count($girdi)) {
-        $kalan = $anahtar[$girdi[$i]];
-        $taban = pow($bolen, $i);
-        /** karşılık gelen değeri çıktıya ekle */
-        $bolunen = $kalan * $taban;
-        (empty($cikti))? $cikti=$bolunen:$cikti = $cikti+$bolunen;
-        $i++;
+    public function encode()
+    {
+        /** anahtar diziyi tanımla */
+        $anahtar = $this->anahtar;
+        $anahtar = str_split($anahtar);
+        /** bölüneni tanımla */
+        $bolunen = $this->girdi;
+        /** böleni tanımla */
+        $bolen = count($anahtar);
+        /** kalanları tanımla */
+        $i = 0;
+        $kalanlar = [];
+        while ($bolunen > 1) {
+            $kalan = $bolunen % $bolen;
+            $bolum = floor($bolunen / $bolen);
+            array_push($kalanlar, $kalan);
+            $bolunen = $bolum;
+            $i++;
+        }
+        /** karakterleri tanımla */
+        $i = count($kalanlar) - 1;
+        while ($i >= 0) {
+            $degerA = $anahtar[$kalanlar[$i]];
+            $i--;
+            /** karşılık gelen değeri çıktıya ekle */
+            (empty($cikti)) ? $cikti = $degerA : $cikti = $cikti . $degerA;
+        }
+        /** çıktıyı döndür */
+        return $cikti;
     }
-    /** çıktıyı döndür */
-    return $cikti;
+
+    public function decode()
+    {
+        /** anahtar diziyi tanımla */
+        $anahtar = $this->anahtar;
+        $anahtar = str_split($anahtar);
+        $anahtar = array_flip($anahtar);
+        /** böleni tanımla */
+        $bolen = count($anahtar);
+        /** karakterleri tanımla */
+        $girdi = $this->girdi;
+        $girdi = str_split($girdi);
+        $girdi = array_reverse($girdi);
+        /** sayıyı hesapla */
+        $i = 0;
+        while ($i < count($girdi)) {
+            $kalan = $anahtar[$girdi[$i]];
+            $taban = pow($bolen, $i);
+            /** karşılık gelen değeri çıktıya ekle */
+            $bolunen = $kalan * $taban;
+            (empty($cikti)) ? $cikti = $bolunen : $cikti = $cikti + $bolunen;
+            $i++;
+        }
+        /** çıktıyı döndür */
+        return $cikti;
+    }
 }
